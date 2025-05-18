bfc568639793d2a46eb2f201f33049281508be18
Precious Nwachokor <PreciousNwachokor@my.unt.edu>
2025-05-18 06:59:31 -0500

    Added nodemon and basic api routes for now

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+// npm run dev
 const express = require("express");
 require("dotenv").config({ path: "./config.env" });
 const connectDB = require("./database.js");
@@ -5,13 +6,37 @@ const connectDB = require("./database.js");
 const app = express();
 const PORT = 8080;
 
+//middleware, basically called every time a request is sent to server
+app.use((req, res, next) => {
+  console.log("Middleware called");
+  console.log("Time:", Date.now());
+  next();
+});
+
+//api routes on the webpage. '/' means homepage
+app.get("/", (req, res) => {
+  res.send("Hello Hammad");
+});
+
+app.post("/", (req, res) => {
+  res.send("Hello Precious");
+});
+
+app.put("/", (req, res) => {
+  res.send("Hello Sayed");
+});
+
+app.delete("/", (req, res) => {
+  res.send("Hello Mr. Nwachokor");
+});
+
 //this function waits for db to connect and then starts server
 const startServer = async () => {
   try {
     const db = await connectDB();
 
     app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
+      console.log(`Server running on http://localhost:${PORT}`);
     });
   } catch (e) {
     console.log("Failed to start server", e);
