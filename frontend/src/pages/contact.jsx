cae0de12b5e025c505ed7531276a32402d3fe086
Precious Nwachokor <PreciousNwachokor@my.unt.edu>
2025-05-29 13:04:44 -0500

    Another boilerplate route

---
+++ frontend/src/pages/contact.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+
+function Contact() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Contact us</h1>
+    </div>
+  );
+}
+
+export default Contact;
